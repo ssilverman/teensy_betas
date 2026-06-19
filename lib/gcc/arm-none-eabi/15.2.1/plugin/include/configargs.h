0a8bd7d9c172679dfa92f65c01cf37359c7a48f1
Shawn Silverman <shawn@pobox.com>
2026-06-19 16:19:14 -0700

    Commit teensy_1.62/toolchain-gccarmnoneeabi-teensy/mac_x86_64

---
+++ lib/gcc/arm-none-eabi/15.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Users/paul/arm/src/gcc/configure --target=arm-none-eabi --prefix=/Users/paul/arm/install --with-gmp=/Users/paul/arm/host-tools --with-mpfr=/Users/paul/arm/host-tools --with-mpc=/Users/paul/arm/host-tools --with-isl=/Users/paul/arm/host-tools --disable-shared --disable-nls --disable-threads --enable-checking=release --enable-languages=c,c++ --with-newlib --with-gnu-as --with-headers=yes --with-gnu-ld --with-native-system-header-dir=/include --with-sysroot=/Users/paul/arm/install/arm-none-eabi --with-multilib-list=aprofile,rmprofile --with-pkgversion=gcc";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" }, { "float", "soft" } };
